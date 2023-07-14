d5dbae4b739a193df2e230e26cb2a5599066afd9
nkserveren26 <norinoritoshi@outlook.jp>
2023-07-14 11:46:59 +0900

    commit

--- lib/cors_api-stack.ts
+++ lib/cors_api-stack.ts
@@ -2,6 +2,9 @@ import * as cdk from 'aws-cdk-lib';
 import { RestApi } from 'aws-cdk-lib/aws-apigateway';
 import { Construct } from 'constructs';
 import { APIGatewayCreator } from './services/apigateway/creator';
+import { Function, Runtime } from 'aws-cdk-lib/aws-lambda';
+import { LambdaCreator } from './services/lambda/creator';
+import { LambdaFunctionParams } from './services/lambda/interfaces';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class CorsApiStack extends cdk.Stack {
@@ -13,6 +16,23 @@ export class CorsApiStack extends cdk.Stack {
       "testAPI",
       "CDKデプロイテスト用のAPI Gateway",
     );
+
+    const lambdaParams: LambdaFunctionParams = {
+      functionName: "APIFunction",
+      codePath: "lambda/APIFunction",
+      runtime: Runtime.PYTHON_3_9,
+      description: "API Gateway用のLambda",
+      handler: "index.handler",
+
+    };
+
+    const lambdaFunction: Function = LambdaCreator.createLambdaFunction(this, lambdaParams);
+
+    const apiParams = {
+      function: "",
+      resource: "getdata",
+      method: "GET",
+    };
 
     
   }
