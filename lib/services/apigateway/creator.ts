9c970ac9b93a141adb712b321732924a70c47941
nkserveren26 <norinoritoshi@outlook.jp>
2023-07-18 15:23:47 +0900

    commit

--- lib/services/apigateway/creator.ts
+++ lib/services/apigateway/creator.ts
@@ -20,7 +20,18 @@ export class APIGatewayCreator {
         apiResource.addMethod(
             "OPTIONS",
             new MockIntegration({
-                integrationResponses: [],
+                integrationResponses: [
+                    {
+                        statusCode: "200",
+                        responseParameters: {
+                            "method.response.header.Access-Control-Allow-Headers":
+                                "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token,X-Amz-User-Agent'",
+                            "method.response.header.Access-Control-Allow-Origin": "'*'",
+                            "method.response.header.Access-Control-Allow-Methods":
+                                "'OPTIONS,GET,PUT,POST,DELETE'",
+                        },
+                    },
+                ],
                 passthroughBehavior: PassthroughBehavior.NEVER,
                 requestTemplates: {
                     "application/json": '{"statusCode": 200}',
